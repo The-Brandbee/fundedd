95c03b21b879e999bfdda6f67acf273f0315371c
The-Brandbee <digital@thebrandbee.com>
2025-04-22 12:09:03 +0530

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,12 @@ import { GoogleTagManager } from "@next/third-parties/google"
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title:
-    "Fundedd",
+    "Payroll Funding Solutions for Staffing Agencies | Fundedd",
   description:
-    "Fundedd",
+    "Explore payroll funding for staffing companies and small businesses. Fundedd offers payroll factoring, financing & staffing agency funding solutions.",
 
   alternates: {
-    canonical: "",
+    canonical: "https://www.fundedd.com/",
   },
   openGraph: {
     images:
