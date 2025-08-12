e330cca830e4d6b8fc27e520787aee0a1d19f9e9
The-Brandbee <digital@thebrandbee.com>
2025-08-12 13:05:26 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -44,7 +44,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function Home() {
   return (
-       <div className="new-home-page-main">     
+       <div className="new-home-page-main--">     
     <GoogleTagManager gtmId="GTM-MFH6JPN5" />                  
       <main>                   
         <Head>
